47df734f17243907792db647c3254aff4b64c7de
Guilherme Seixas <guilhermeseixasabreu@gmail.com>
2022-05-24 04:01:45 -0300

    routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import "normalize.css"
+import 'normalize.css';
 import './index.css';
-import Cardapio from './pages/Cardapio';
+
+import Router from './routes'
+
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Cardapio/>
+    <Router />
   </React.StrictMode>
 );
 
--- src/pages/Cardapio/Ordenador/index.tsx
+++ src/pages/Cardapio/Ordenador/index.tsx
@@ -1,8 +1,8 @@
-import styles from "./Ordenador.module.scss";
+import styles from './Ordenador.module.scss';
 import opcoes from './opcoes.json';
-import React, { useState } from "react";
-import classNames from "classnames";
-import { MdKeyboardArrowUp, MdKeyboardArrowDown } from 'react-icons/md'
+import React, { useState } from 'react';
+import classNames from 'classnames';
+import { MdKeyboardArrowUp, MdKeyboardArrowDown } from 'react-icons/md';
 
 
 interface Props{
@@ -11,29 +11,29 @@ interface Props{
 }
 
 export default function Ordenador({ ordenador, setOrdenador }: Props) {
-    const [aberto, setAberto] = useState(false);
-    const nomeOrdenador = ordenador && opcoes.find(opcao => opcao.value === ordenador)?.nome
-    return (
-        <button className={classNames({
-            [styles.ordenador]: true,
-            [styles["ordenador--ativo"]]: ordenador !== "",
-        })} 
-        onClick={()=> setAberto(!aberto)}
-        onBlur={()=> setAberto(false)}
-        >
-            <span>{nomeOrdenador || "Ordenar Por"}</span>
-            {aberto ? < MdKeyboardArrowUp size={20}/> : < MdKeyboardArrowDown size={20}/>}
-            <div className={classNames({
-                [styles.ordenador__options]: true,
-                [styles['ordenador__options--ativo']]: aberto,
-            })}>
-                {opcoes.map((opcao) => (
-                    <div className={styles.ordenador__option} key={opcao.value}
-                    onClick={()=> setOrdenador(opcao.value)}>
-                        {opcao.nome}
-                    </div>
-                ))}
-            </div>
-        </button>
-    )
+  const [aberto, setAberto] = useState(false);
+  const nomeOrdenador = ordenador && opcoes.find(opcao => opcao.value === ordenador)?.nome;
+  return (
+    <button className={classNames({
+      [styles.ordenador]: true,
+      [styles['ordenador--ativo']]: ordenador !== '',
+    })} 
+    onClick={()=> setAberto(!aberto)}
+    onBlur={()=> setAberto(false)}
+    >
+      <span>{nomeOrdenador || 'Ordenar Por'}</span>
+      {aberto ? < MdKeyboardArrowUp size={20}/> : < MdKeyboardArrowDown size={20}/>}
+      <div className={classNames({
+        [styles.ordenador__options]: true,
+        [styles['ordenador__options--ativo']]: aberto,
+      })}>
+        {opcoes.map((opcao) => (
+          <div className={styles.ordenador__option} key={opcao.value}
+            onClick={()=> setOrdenador(opcao.value)}>
+            {opcao.nome}
+          </div>
+        ))}
+      </div>
+    </button>
+  );
 }
